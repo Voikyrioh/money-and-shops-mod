8ecdbbcfbf1c926462ca409bce007b2597190251
Voikyrioh <voikyrioh@gmail.com>
2021-07-06 13:12:07 +0200

    [feat] Fixed shop in a box sounds, settings and craft

--- src/main/java/fr/voikyrioh/voikymasm/ShopInABlock.java
+++ src/main/java/fr/voikyrioh/voikymasm/ShopInABlock.java
@@ -2,15 +2,26 @@ package fr.voikyrioh.voikymasm;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.LiteralText;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import org.jetbrains.annotations.Nullable;
 
 public class ShopInABlock extends Block {
+    public static final DirectionProperty FACING;
+
     public ShopInABlock(Settings settings) {
         super(settings);
     }
@@ -18,9 +29,36 @@ public class ShopInABlock extends Block {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (!world.isClient) {
-            player.sendMessage(new LiteralText("Hello, world!"), false);
+            world.playSound(null, pos, SoundEvents.ENTITY_VILLAGER_TRADE, SoundCategory.BLOCKS, 1f, 1f);
         }
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+        super.appendProperties(builder);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING,ctx.getPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+        world.playSound(null, pos, SoundEvents.ENTITY_VILLAGER_CELEBRATE, SoundCategory.BLOCKS, 1f, 1f);
+        super.onPlaced(world, pos, state, placer, itemStack);
+    }
+
+    @Override
+    public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
+        world.playSound(null, pos, SoundEvents.ENTITY_VILLAGER_HURT, SoundCategory.BLOCKS, 1f, 1f);
+        super.onBroken(world, pos, state);
+    }
+
+    static {
+        FACING = HorizontalFacingBlock.FACING;
     }
 }
--- src/main/java/fr/voikyrioh/voikymasm/VoikyriohMoneyAndShopsMod.java
+++ src/main/java/fr/voikyrioh/voikymasm/VoikyriohMoneyAndShopsMod.java
@@ -3,22 +3,26 @@ package fr.voikyrioh.voikymasm;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class VoikyriohMoneyAndShopsMod implements ModInitializer {
-	public static final Block shopInABlock = new ShopInABlock(FabricBlockSettings.of(Material.WOOD).strength(4.0f));
+	public static final Block shopInABlock = new ShopInABlock(
+			FabricBlockSettings
+					.of(Material.WOOD)
+					.sounds(BlockSoundGroup.WOOD)
+					.breakByTool(FabricToolTags.AXES, 1)
+					.strength(1.0f, 30.0f)
+					.luminance(1)
+	);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		Registry.register(Registry.BLOCK, new Identifier("voikymasm", "shop_in_a_block"), shopInABlock);
 		Registry.register(Registry.ITEM, new Identifier("voikymasm", "shop_in_a_block"), new BlockItem(shopInABlock, new FabricItemSettings().group(ItemGroup.MISC)));
 	}
